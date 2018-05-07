3f1a40f273d4226c7d3300003aa4d7c1ef17e775
Ivan Markin <sw@nogoegst.net>
2018-05-07 13:25:02 +0000

    Move codes to subpackages, implement AMP encoder

--- client.go
+++ client.go
@@ -8,30 +8,16 @@
 package amper
 
 import (
-	"bytes"
-	"crypto/rand"
-	"encoding/base64"
-	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path"
 	"strings"
 
-	"golang.org/x/net/html"
+	ampcodec "github.com/nogoegst/amper/codec/amp"
+	getcodec "github.com/nogoegst/amper/codec/get"
 )
-
-// Produce a random ID as a URL-safe Base64 string.
-func randomID() string {
-	b := make([]byte, 10)
-	_, err := io.ReadFull(rand.Reader, b)
-	if err != nil {
-		panic(err)
-	}
-	return base64.RawURLEncoding.EncodeToString(b)
-}
 
 const (
 	CDNDomain = "cdn.ampproject.org"
@@ -42,70 +28,6 @@ func hostToAMPHost(h string) string {
 	h = strings.Replace(h, "-", "--", -1)
 	h = strings.Replace(h, ".", "-", -1)
 	return h + "." + CDNDomain
-}
-
-func getAttribute(n *html.Node, key string) (string, bool) {
-	for _, attr := range n.Attr {
-		if attr.Key == key {
-			return attr.Val, true
-		}
-	}
-	return "", false
-}
-
-func getNodeByID(n *html.Node, id string) *html.Node {
-	if n.Type == html.ElementNode {
-		s, ok := getAttribute(n, "id")
-		if ok && s == id {
-			return n
-		}
-	}
-
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		result := getNodeByID(c, id)
-		if result != nil {
-			return result
-		}
-	}
-	return nil
-}
-
-// extractData extracts payload from an AMP page body r.
-func extractData(r io.Reader) (io.Reader, error) {
-	doc, err := html.Parse(r)
-	if err != nil {
-		return nil, err
-	}
-	n := getNodeByID(doc, "data")
-	if n == nil {
-		err = errors.New("no element with this ID")
-		return nil, err
-	}
-	// The node found but there is no child.
-	if n.FirstChild == nil {
-		return bytes.NewReader(nil), nil
-	}
-	data := n.FirstChild.Data
-	b, err := base64.RawURLEncoding.DecodeString(data)
-	if err != nil {
-		return nil, err
-	}
-	br := bytes.NewReader(b)
-	return br, nil
-}
-
-// assemblePath encodes data from reader r into URL path.
-// The format is "/{random}/{payload}" where random is
-// a random string to disable cache, and payload is the payload
-// encoded into URL-safe Base64.
-func assemblePath(r io.Reader) (string, error) {
-	slug := randomID()
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		return "", err
-	}
-	req := base64.RawURLEncoding.EncodeToString(data)
-	return path.Join(slug, req), nil
 }
 
 // Client desribes a client state.
@@ -122,7 +44,7 @@ type Client struct {
 // RoundTrip writes data from reader r to the server and returns
 // reply from the server.
 func (c *Client) RoundTrip(r io.Reader) (io.Reader, error) {
-	reqPath, err := assemblePath(r)
+	reqPath, err := getcodec.Encode(r)
 	if err != nil {
 		return nil, err
 	}
@@ -151,7 +73,7 @@ func (c *Client) RoundTrip(r io.Reader) (io.Reader, error) {
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("http status code %d", resp.StatusCode)
 	}
-	data, err := extractData(resp.Body)
+	data, err := ampcodec.NewDecoder(resp.Body)
 	if err != nil {
 		return nil, err
 	}
--- server.go
+++ server.go
@@ -8,48 +8,15 @@
 package amper
 
 import (
-	"bytes"
-	"encoding/base64"
-	"errors"
 	"io"
 	"net/http"
-	"strings"
-)
 
-var (
-	ampHeader = []byte(`<!doctype html>
-<html amp>
-  <head>
-    <meta charset="utf-8">
-    <title>amp</title>
-    <link rel="canonical" href="https://ampproject.org" />
-    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-    <style>body {opacity: 0}</style><noscript><style>body {opacity: 1}</style></noscript>
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
-  </head>
-  <body>
-    <p>In varietate concordia</p>
-    <pre id="data">`)
-	ampTrailer = []byte(`</pre>
-  </body>
-</html>`)
+	ampcodec "github.com/nogoegst/amper/codec/amp"
+	getcodec "github.com/nogoegst/amper/codec/get"
 )
 
 type Handler interface {
 	Handle(w io.Writer, r io.Reader) error
-}
-
-// decodeRequest decodes request data from the path.
-func decodeRequest(path string) ([]byte, error) {
-	sp := strings.SplitN(path, "/", 3)
-	if len(sp) < 3 {
-		return nil, errors.New("path is too short")
-	}
-	b, err := base64.RawURLEncoding.DecodeString(sp[2])
-	if err != nil {
-		return nil, err
-	}
-	return b, nil
 }
 
 var (
@@ -68,34 +35,22 @@ type Server struct {
 }
 
 func (ah *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// We do not throw any HTTP errors because clients are not going
-	// to get them anyway (because of the cache middleware).
-	var invalid = false
-	data, err := decodeRequest(r.URL.Path)
-	if err != nil {
-		invalid = true
-	}
 	// We want to give a hint to AMP cache that this is request
 	// will not be used anymore.
 	w.Header().Set("Cache-Control", "private, max-age=0")
 
-	// Write AMP header
-	_, err = w.Write(ampHeader)
+	// We always write AMP page even if it has no useful data.
+	enc := ampcodec.NewEncoder(w)
+	defer enc.Close()
+	// We do not throw any HTTP errors because clients are not going
+	// to get them anyway (because of the cache middleware).
+	req, err := getcodec.Decode(r.URL.Path)
+	if err != nil {
+		return
+	}
+	err = ah.Handler.Handle(enc, req)
 	if err != nil {
 		return
 	}
 
-	// Write the data itself. If data is invalid we send
-	// empty HTML tag which will be trimmed by AMP as non-printable.
-	if !invalid {
-		enc := base64.NewEncoder(base64.RawURLEncoding, w)
-		ah.Handler.Handle(enc, bytes.NewReader(data))
-		enc.Close()
-	}
-
-	// Write AMP trailer.
-	_, err = w.Write(ampTrailer)
-	if err != nil {
-		return
-	}
 }
