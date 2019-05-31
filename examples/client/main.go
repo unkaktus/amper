c8191a55d95935183ef7a0a2e4dbab162056b4f8
Ivan Markin <sw@nogoegst.net>
2019-05-31 12:22:13 +0000

    Extend example client to echo more data and work a bit like ping(1)

--- examples/client/main.go
+++ examples/client/main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"bytes"
+	"crypto/rand"
+	"io"
 	"io/ioutil"
+	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/nogoegst/amper"
 	"github.com/rs/zerolog/log"
 )
+
+func ping(c *amper.Client) {
+	req := &bytes.Buffer{}
+	io.CopyN(req, rand.Reader, 1550)
+	reqData := req.Bytes()
+
+	resp, err := c.RoundTrip(req)
+	if err != nil {
+		log.Fatal().Err(err).Msg("perform round trip")
+	}
+
+	respData, err := ioutil.ReadAll(resp)
+	if err != nil {
+		log.Fatal().Err(err).Msg("read out response")
+	}
+	if diff := cmp.Diff(respData, reqData); diff != "" {
+		log.Fatal().Msgf("response differs: %s", diff)
+	}
+}
 
 func main() {
 	c := &amper.Client{
@@ -14,20 +37,13 @@ func main() {
 		Front: "www.google.com",
 	}
 
-	req := &bytes.Buffer{}
-	_, err := req.Write([]byte("request data"))
-	if err != nil {
-		log.Fatal().Err(err).Msg("write payload to a buffer")
+	ticker := time.NewTicker(1 * time.Second)
+	for range ticker.C {
+		go func() {
+			start := time.Now()
+			ping(c)
+			rtt := time.Since(start)
+			log.Info().Str("rtt", rtt.String()).Msg("got response")
+		}()
 	}
-
-	resp, err := c.RoundTrip(req)
-	if err != nil {
-		log.Fatal().Err(err).Msg("perform round trip")
-	}
-
-	data, err := ioutil.ReadAll(resp)
-	if err != nil {
-		log.Fatal().Err(err).Msg("read out response")
-	}
-	log.Printf("response: %s", data)
 }
